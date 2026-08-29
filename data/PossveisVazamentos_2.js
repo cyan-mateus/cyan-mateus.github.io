73690c73e5a4327a9ff8a6213fac28a0e7c76cd5
Mateus Porto <mateus.porto@cyan-agro.com>
2026-08-29 18:02:06 -0300

    Update map files

--- data/PossveisVazamentos_2.js
+++ data/PossveisVazamentos_2.js
@@ -1,1 +1,26 @@
-var json_PossveisVazamentos_2 = {"type":"FeatureCollection","name":"PossveisVazamentos_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1","controle":"Possível Vazamento"},"geometry":{"type":"Point","coordinates":[-54.772688242633265,-17.580616684009726]}},{"type":"Feature","properties":{"fid":"2","controle":"Possível Vazamento"},"geometry":{"type":"Point","coordinates":[-54.776680982502533,-17.578563788685145]}},{"type":"Feature","properties":{"fid":"6","controle":"Possível Vazamento"},"geometry":{"type":"Point","coordinates":[-54.771997174746311,-17.578313983480218]}},{"type":"Feature","properties":{"fid":"7","controle":"Possível Vazamento"},"geometry":{"type":"Point","coordinates":[-54.770234728799522,-17.579752509965274]}},{"type":"Feature","properties":{"fid":"8","controle":"Possível Vazamento"},"geometry":{"type":"Point","coordinates":[-54.771253038590558,-17.580742687553371]}},{"type":"Feature","properties":{"fid":"9","controle":"Possível Vazamento"},"geometry":{"type":"Point","coordinates":[-54.771323878770644,-17.580621087687792]}},{"type":"Feature","properties":{"fid":"10","controle":"Possível Vazamento"},"geometry":{"type":"Point","coordinates":[-54.772174858845673,-17.581529012068923]}},{"type":"Feature","properties":{"fid":"11","controle":"Possível Vazamento"},"geometry":{"type":"Point","coordinates":[-54.7775980865731,-17.577142402792767]}},{"type":"Feature","properties":{"fid":"12","controle":"Possível Vazamento"},"geometry":{"type":"Point","coordinates":[-54.775017281149879,-17.579539389025381]}},{"type":"Feature","properties":{"fid":"13","controle":"Possível Vazamento"},"geometry":{"type":"Point","coordinates":[-54.776253018695876,-17.585584095397241]}}]}
+var json_PossveisVazamentos_2 = {
+"type": "FeatureCollection",
+"name": "a",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{"type":"Feature","properties":{"fid":1,"controle":"1"},"geometry":{"type":"Point","coordinates":[-54.772688242633265,-17.580616684009726]}},
+{"type":"Feature","properties":{"fid":2,"controle":"2"},"geometry":{"type":"Point","coordinates":[-54.776680982502533,-17.578563788685145]}},
+{"type":"Feature","properties":{"fid":6,"controle":"3"},"geometry":{"type":"Point","coordinates":[-54.771997174746311,-17.578313983480218]}},
+{"type":"Feature","properties":{"fid":7,"controle":"4"},"geometry":{"type":"Point","coordinates":[-54.770234728799522,-17.579752509965274]}},
+{"type":"Feature","properties":{"fid":8,"controle":"5"},"geometry":{"type":"Point","coordinates":[-54.771253038590558,-17.580742687553371]}},
+{"type":"Feature","properties":{"fid":9,"controle":"6"},"geometry":{"type":"Point","coordinates":[-54.771323878770644,-17.580621087687792]}},
+{"type":"Feature","properties":{"fid":11,"controle":"8"},"geometry":{"type":"Point","coordinates":[-54.7775980865731,-17.577142402792767]}},
+{"type":"Feature","properties":{"fid":12,"controle":"9"},"geometry":{"type":"Point","coordinates":[-54.775017281149879,-17.579539389025381]}},
+{"type":"Feature","properties":{"fid":13,"controle":"10"},"geometry":{"type":"Point","coordinates":[-54.776253018695876,-17.585584095397241]}},
+{"type":"Feature","properties":{"fid":15,"controle":null},"geometry":{"type":"Point","coordinates":[-54.774600645254473,-17.575047802133479]}},
+{"type":"Feature","properties":{"fid":17,"controle":null},"geometry":{"type":"Point","coordinates":[-54.771369323723945,-17.576873049849354]}},
+{"type":"Feature","properties":{"fid":21,"controle":null},"geometry":{"type":"Point","coordinates":[-54.775984479005963,-17.583920465175222]}},
+{"type":"Feature","properties":{"fid":27,"controle":null},"geometry":{"type":"Point","coordinates":[-54.77729812923333,-17.583580179479902]}},
+{"type":"Feature","properties":{"fid":28,"controle":null},"geometry":{"type":"Point","coordinates":[-54.777493404274246,-17.583519717151894]}},
+{"type":"Feature","properties":{"fid":29,"controle":null},"geometry":{"type":"Point","coordinates":[-54.776600733103223,-17.58362524676847]}},
+{"type":"Feature","properties":{"fid":30,"controle":null},"geometry":{"type":"Point","coordinates":[-54.773775903786706,-17.588224581857158]}},
+{"type":"Feature","properties":{"fid":32,"controle":null},"geometry":{"type":"Point","coordinates":[-54.773521815664921,-17.586804252900738]}},
+{"type":"Feature","properties":{"fid":34,"controle":null},"geometry":{"type":"Point","coordinates":[-54.772516675935258,-17.580150469016051]}},
+{"type":"Feature","properties":{"fid":36,"controle":null},"geometry":{"type":"Point","coordinates":[-54.77362530334613,-17.585444112882531]}}
+]
+}
